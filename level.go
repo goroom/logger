5c1eb7bafef3a9b729bc44ff2e30c135b9f106cc
lesterpang <252228180@qq.com>
2018-10-10 14:21:56 +0800

    Add "PaeseLevel" function
    Signed-off-by: lesterpang <252228180@qq.com>

--- level.go
+++ level.go
@@ -14,6 +14,27 @@ func (l Level) ConsoleColorString() string {
 	return fmt.Sprintf("\033[;%dm%s\033[0m", l.ConsoleColorNum(), l.String())
 }
 
+func ParseLevel(str string) Level {
+	switch strings.ToLower(str) {
+	case "all":
+		return ALL
+	case "debug":
+		return DEBUG
+	case "info":
+		return INFO
+	case "warn", "warning":
+		return WARN
+	case "error", "err":
+		return ERROR
+	case "fatal":
+		return FATAL
+	case "off":
+		return OFF
+	default:
+		return ALL
+	}
+}
+
 func (l Level) ConsoleColorNum() int {
 	switch l {
 	case DEBUG:
