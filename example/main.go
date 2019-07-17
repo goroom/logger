b23a01b9c7aa87c86d5e942d6b4a7de877a5c289
lesterpang <252228180@qq.com>
2019-07-17 20:40:40 +0800

    兼容

--- example/main.go
+++ example/main.go
@@ -12,15 +12,14 @@ func main() {
 	logger.GetDefaultLogger().Info(path.Base(os.Args[0]))
 
 	logger.InitDefaultLogger(
-		logger.WithFileLevel(logger.INFO),
-		logger.WithFileSize(logger.MB/10),
-		logger.WithFileSplit(logger.DefaultFileNoSplit),
+		logger.WithFileLevel(logger.ALL),
+		logger.WithFileSplit(logger.DefaultFileSplitByMinute),
 		logger.WithCallBack(func(f *logger.Format) {
 			//fmt.Println(f.Time)
 		}),
 	)
 
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 100; i++ {
 		logger.Debug(time.Now().UnixNano())
 		logger.Info(time.Now().UnixNano())
 		logger.Warn(time.Now().UnixNano())
@@ -30,7 +29,7 @@ func main() {
 		logger.Infof("Infof %v", i)
 		logger.Warnf("Warnf %v", i)
 		logger.Errorf("Errorf %v", i)
-		time.Sleep(time.Millisecond * 1)
+		time.Sleep(time.Second * 1)
 	}
 	logger.Wait()
 }
